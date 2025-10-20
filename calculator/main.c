f2d292ec34b62e0fb2c58afabc9974b41f5bffaa
sanin564 <saninthottungalhere@gmail.com>
2025-10-20 19:59:10 +0530

    iteration added

--- calculator/main.c
+++ calculator/main.c
@@ -12,62 +12,70 @@ int main(int argc, char const *argv[])
 {
     int choice;
     float num1 = 0, num2 = 0, result = 0;
-
-    printf("\n-------------------------------\n");
-    printf("Welcome to the Calculator\n");
-
-    printf("\nChoose one of the following options: \n");
-    printf(""
-           "1. Add\n"
-           "2. Substract\n"
-           "3. Multiply\n"
-           "4. Divide\n"
-           "5. Modulus\n"
-           "6. Power\n"
-           "7. Exit\n"
-           "");
-
-    printf("Now, Enter your choice: ");
-    scanf(" %d", &choice);
-
-    if (choice < 1 || choice > 7)
+    do
     {
-        printf("\n\nInvalid Option Selected!!\n");
-        return 0;
-    }
 
-    printf("\n\nPlease enter your first number: ");
-    scanf(" %f", &num1);
+        printf("\n-------------------------------\n");
+        printf("Welcome to the Calculator\n");
 
-    printf("Please enter your second number: ");
-    scanf(" %f", &num2);
+        printf("\nChoose one of the following options: \n");
+        printf(""
+               "1. Add\n"
+               "2. Substract\n"
+               "3. Multiply\n"
+               "4. Divide\n"
+               "5. Modulus\n"
+               "6. Power\n"
+               "7. Exit\n"
+               "");
 
-    switch (choice)
-    {
-    case 1:
-        result = sum(num1, num2);
-        break;
-    case 2:
-        result = substraction(num1, num2);
-        break;
-    case 3:
-        result = multiplication(num1, num2);
-        break;
-    case 4:
-        result = division(num1, num2);
-        break;
-    case 5:
-        result = modulus(num1, num2);
-        break;
-    case 6:
-        result = power(num1, num2);
-        break;
-    default:
-        printf("\nInvalid option Selected\n");
-        return 0;
-    }
+        printf("Now, Enter your choice: ");
+        scanf(" %d", &choice);
 
-    printf("\n\nThe result of the operation is: %f\n", result);
+        if (choice < 1 || choice > 7)
+        {
+            printf("\n\nInvalid Option Selected!!\n");
+            return 0;
+        }
+
+        if (choice == 7)
+        {
+            return 0;
+        }
+
+        printf("\n\nPlease enter your first number: ");
+        scanf(" %f", &num1);
+
+        printf("Please enter your second number: ");
+        scanf(" %f", &num2);
+
+        switch (choice)
+        {
+        case 1:
+            result = sum(num1, num2);
+            break;
+        case 2:
+            result = substraction(num1, num2);
+            break;
+        case 3:
+            result = multiplication(num1, num2);
+            break;
+        case 4:
+            result = division(num1, num2);
+            break;
+        case 5:
+            result = modulus(num1, num2);
+            break;
+        case 6:
+            result = power(num1, num2);
+            break;
+        default:
+            printf("\nInvalid option Selected\n");
+            return 0;
+        }
+
+        printf("\n\nThe result of the operation is: %f\n", result);
+    } while (choice != 7);
 
     return 0;
 }
