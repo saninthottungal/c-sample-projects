b1eb0be20f141caae0e5292e5393b06907af16d8
sanin564 <saninthottungalhere@gmail.com>
2025-10-24 11:39:56 +0530

    registration setup basic

--- user_management/main.c
+++ user_management/main.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 
+#define MAX_USERS 50
+
+typedef struct
+{
+    char username[20];
+    char password[20];
+} User;
+
+void registerUser(User arr[], int index);
+
 int main(int argc, char const *argv[])
 {
+    User users[MAX_USERS] = {0};
+    int counter = 0;
+
     while (1)
     {
         int userSelection;
         printf("\n1. Register\n2. Login\n3. Exit\n");
+        printf("Select an option: ");
         scanf(" %d", &userSelection);
 
         switch (userSelection)
         {
         case 1:
-            printf("Registration initiated\n");
+            printf("Register a new user\n");
+            registerUser(users, counter);
+
             return 0;
 
         case 2:
@@ -24,4 +40,13 @@ int main(int argc, char const *argv[])
         }
     }
     return 0;
+}
+
+void registerUser(User arr[], int index)
+{
+    User user;
+    printf("\nEnter username(MAX 20 characters): ");
+    scanf(" %s", user.username);
+    printf("Enter password(MAX 20 characters): ");
+    scanf(" %s", user.password);
 }
