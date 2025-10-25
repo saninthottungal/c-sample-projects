0c36304e53d3ed8e3aa5c59845c7b598e7b2dd55
sanin <saninthottungalhere@gmail.com>
2025-10-25 20:20:14 +0530

    move validator added

--- library_management/tic_tac_toe/tic_tac_toe.c
+++ library_management/tic_tac_toe/tic_tac_toe.c
@@ -1,16 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+typedef enum
+{
+    PX,
+    P0,
+} Player;
 
 void drawBoard(char board[3][3]);
+void takeTurn(Player turn, int *x, int *y);
+int validateTake(char board[3][3], int x, int y);
 
 int main()
 {
+    Player turn = PX;
     char board[3][3] = {
         {' ', ' ', ' '},
         {' ', ' ', ' '},
         {' ', ' ', ' '},
     };
 
-    drawBoard(board);
+    while (1)
+    {
+        int x = 0, y = 0, isValidMove = 1;
+        drawBoard(board);
+        if (!isValidMove)
+        {
+            printf("!!! Not a valid Move !!!\n");
+        }
+
+        takeTurn(turn, &x, &y);
+        isValidMove = validateTake(board, x, y);
+        system("clear");
+    }
 
     return 0;
 }
@@ -38,4 +59,17 @@ void drawBoard(char board[3][3])
     }
 
     printf("\n\n");
+}
+
+void takeTurn(Player turn, int *x, int *y)
+{
+    char player = turn == PX ? 'X' : '0';
+
+    printf("turn of %c, play! (1-3)(1-3): ", player);
+    scanf("%d %d", x, y);
+}
+
+int validateTake(char board[3][3], int x, int y)
+{
+    return board[x][y] != ' ';
 }
