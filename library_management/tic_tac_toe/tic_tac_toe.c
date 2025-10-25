26dae0e5b283317259836083fc3da521a23f8456
sanin <saninthottungalhere@gmail.com>
2025-10-25 19:29:03 +0530

    drawing board done

---
+++ library_management/tic_tac_toe/tic_tac_toe.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void drawBoard(char board[3][3]);
+
+int main()
+{
+    char board[3][3] = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+    };
+
+    drawBoard(board);
+
+    return 0;
+}
+
+void drawBoard(char board[3][3])
+{
+    printf("\n\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (i != 0)
+        {
+            printf("\n-----------\n");
+        }
+
+        for (int j = 0; j < 3; j++)
+        {
+            printf(" %c ", board[i][j]);
+            if (j < 2)
+            {
+                printf("|");
+            }
+        }
+    }
+
+    printf("\n\n");
+}
